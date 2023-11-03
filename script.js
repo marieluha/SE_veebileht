fe6cdbbdd7f1e7f72fdf1c3093e0227c248e9243
Marie Reti Luha <marieret@ut.ee>
2023-11-03 20:55:06 +0200

    pannkoogid

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// allikas kust sain
+
 function kerigeRetseptiJuurde() {
     const retseptElement = document.getElementById("retsept1");
     retseptElement.scrollIntoView({ behavior: "smooth" });
